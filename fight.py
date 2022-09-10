92f3b3ae5728338ef9b3e03aecbab734235df5ff
Fran Osores <fosores@linets.cl>
2022-09-10 18:18:54 -0400

    ADD turns for both players and condition to end the figth done

--- fight.py
+++ fight.py
@@ -3,7 +3,9 @@ from players import Player
 from utils import check_who_has_the_first_turn, get_message_and_energy_to_discount
 
 import json
+import time
 
+MAX_TURNS = 5
 
 input_file = open("input.json")
 data = json.load(input_file)
@@ -29,19 +31,30 @@ second_player = player1 if first_player != player1 else player2
 # Start Fight!
 
 turn = 0
-while first_player.is_alive and second_player.is_alive and turn < 5:
+while first_player.is_alive and second_player.is_alive and turn < MAX_TURNS:
+    time.sleep(1)
     first_player_movement = first_player.movements[turn]
     first_player_hit = first_player.hits[turn]
     energy_to_discount = get_message_and_energy_to_discount(first_player.name, first_player_movement, first_player_hit)
-    print(f"ENERGY TO Disc {energy_to_discount}")
+
+    if player2.energy > 0 and player2.energy - energy_to_discount > 0:
+        player2.energy = player2.energy - energy_to_discount
+    else:
+        print(f"HA GANADO {player1.name} y aun le quedaban {player1.energy} puntos de energia!")
+        player2.is_alive = False
+
+    #### END PLAYER1'S TURN ###
+
+    second_player_movement = second_player.movements[turn]
+    second_player_hit = second_player.hits[turn]
+    energy_to_discount = get_message_and_energy_to_discount(second_player.name, second_player_movement, second_player_hit)
+
+    if player1.energy > 0 and player1.energy - energy_to_discount > 0:
+        player1.energy = player1.energy - energy_to_discount
+    else:
+        print(f"HA GANADO {player2.name} y aun le quedaban {player2.energy} puntos de energia!")
+        player1.is_alive = False
+
+    ### END PLAYER2'S TURN ###
+
     turn += 1
-
-
-
-
-
-
-
-
-
-
